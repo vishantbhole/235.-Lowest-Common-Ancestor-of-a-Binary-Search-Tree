6b70bebf634b1024929ebdf724c3ab1825580fe2
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-10-18 21:28:13 -0500

    Implement binary tree builder function
    Added a helper function to build a binary tree from a list.

--- lowestCommonAncestor.py
+++ lowestCommonAncestor.py
@@ -16,3 +16,21 @@ class Solution:
                 curr = curr.left
             else:
                 return curr
+
+# Helper function to build a binary tree from a list (BFS style)
+def buildTree(values):
+    if not values:
+        return None
+
+    nodes = [TreeNode(v) if v is not None else None for v in values]
+    kids = nodes[::-1]
+    root = kids.pop()
+
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+
+    return root
